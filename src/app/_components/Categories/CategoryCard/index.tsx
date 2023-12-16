0c33a289d965f3df1889c9aaa67a77f948786776
Shing Him Ho <shinghimho@ShingdeMacBook-Pro-6.local>
2023-12-16 19:40:33 +0800

    disabled prettier/prettier

--- src/app/_components/Categories/CategoryCard/index.tsx
+++ src/app/_components/Categories/CategoryCard/index.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 import React from 'react'
 
 import classes from './index.module.scss'
@@ -7,16 +7,16 @@ import { Category, Media } from '../../../../payload/payload-types'
 import { useFilter } from '../../../_providers/Filter'
 
 type CategoryCardProps = {
-    category: Category
+  category: Category
 }
 
 const CategoryCard = ({category} :CategoryCardProps) => {
-    const media = category.media as Media
-    const { setCategoryFilters } = useFilter()
+  const media = category.media as Media
+  const { setCategoryFilters } = useFilter()
 
   return (
-    <Link href='/products' className={classes.card} style={{ backgroundImage: `url(${media.url})`}} onClick={() => setCategoryFilters([category.id])}>
-        <p className={classes.title}>{category.title}</p>
+    <Link href="/products" className={classes.card} style={{ backgroundImage: `url(${media.url})`}} onClick={() => setCategoryFilters([category.id])}>
+      <p className={classes.title}>{category.title}</p>
     </Link>
   )
 }
